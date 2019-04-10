12026de0ec82d89f81e6ed661b14dbcc26fcab3c
Nick Anderson <nickanderson@gmail.com>
2019-04-10 08:35:25 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
+import BarChart from "./Components/BarChart";
+import { range as d3Range } from "d3";
 
-import "./styles.css";
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: d3Range(34).map(Math.random),
+    currentIndex: null
+  };
+
+  addData = () =>
+    this.setState({
+      data: [...this.state.data, Math.random()]
+    });
+
+  setCurrentIndex = currentIndex =>
+    this.setState({
+      currentIndex
+    });
+
+  render() {
+    const { data, currentIndex } = this.state;
+
+    return (
+      <MuiThemeProvider>
+        <div style={styles}>
+          <RaisedButton
+            label={"Click Me"}
+            secondary={true}
+            onClick={this.addData}
+          />
+          <p>
+            {data.length} datapoints
+            <br />
+          </p>
+          <svg width="100%" height="500">
+            <BarChart
+              data={data}
+              width={500}
+              height={250}
+              x={0}
+              y={0}
+              highlightBar={this.setCurrentIndex}
+              highlightedBar={currentIndex}
+            />
+          </svg>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(<App />, document.getElementById("root"));
